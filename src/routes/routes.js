107d59190a907b76e08f1450c15bec4293e210e2
Eduardo O. Tomás <jeduardootomas@gmail.com>
2025-12-05 20:27:45 -0600

    feat(routes) Add routes for users, equipment, and history

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,21 @@
 import { createBrowserRouter } from 'react-router'
 import App from '../App'
+import { Home } from '../components/Home'
+import { HistoryPage } from '../components/HistoryPage'
+import { UsersPage } from '../components/UsersPage'
+import { EquipmentPage } from '../components/EquipmentPage'
+import { NotFound } from '../components/NotFound'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    Component: App
+    Component: App,
+    ErrorBoundary: NotFound,
+    children: [
+      { index: true, Component: Home },
+      { path: 'history', Component: HistoryPage },
+      { path: 'users', Component: UsersPage },
+      { path: 'equipment', Component: EquipmentPage },
+    ]
   },
 ])
